6d13568f0c166a4cc52c32013958ca8bb2006b2b
STÉPHANE IRIE <vangnybi@gmail.com>
2025-07-23 20:19:01 +0000

    favicon add

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
